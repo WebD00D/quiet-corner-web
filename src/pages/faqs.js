c96822cef81b60ff34da041c56c856a76cf6681c
Christian <rva.christian91@gmail.com>
2018-02-01 16:59:13 -0800

    adds new faqs

--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -11,19 +11,24 @@ const SecondPage = () => (
           <div className="hero-cta__title">Have a Question?</div>
         </div>
         <div className="faq-wrapper">
+        <Faq
+          headline="What is Quiet Corner?"
+          paragraphOne="It’s a micropayment paywall that allows you to buy stories devoid of advertising without having to lock in to a contract with a publisher. "
+
+        />
           <Faq
             headline="Why do I need to use Quiet Corner?"
             paragraphOne="You don’t. Media is in a very tricky place, however. Once upon a time our models were a mix of advertising and reader contribution. Recently, most of us have lost the war to Google and Facebook. We spent thousands trying to build our audiences with them and now have to pay to turn them on. We all make mistakes, but can you believe some of us embedded entire Instant Articles in Facebook with the promise of healthy ad commissions? "
-            paragraphTwo="We can’t change the past but we can shape the future. And, that means to stay alive and thrive, we need to be self-funded and run our media businesses with a mix of advertising and reader contributions. And, if we were really honest with ourselves, we’d likely all choose to have our sites be pure and devoid of the clutter of advertising. "
-            paragraphThree="Picture this, to those of us in print media: your most loyal reader subscribes to your print edition, or wanders down to the newsstand to pick up the latest issue. They sit back, consume the magazine and that afternoon, they go browsing the internet. Here, they find the cover story + main feature in its entirety, identical except for how it’s displayed: every word, every photo, every caption. Is this how we look after our most loyal customer?"
+            paragraphTwo="We can’t change the past but we can shape the future. And, that means to stay alive and thrive, we need to be self-funded and run our media businesses with a mix of advertising and reader contributions. And, if we were really honest with ourselves, we’d likely all choose to have our sites be pure and devoid of the clutter. "
+            paragraphThree="Picture those of us in print media: our most loyal reader subscribes to your print edition, or wanders down to the newsstand to pick up the latest issue. They sit back, consume the magazine and that afternoon, they go browsing the internet. Here, they find the cover story + main feature in its entirety, identical except for how it’s displayed: every word, every photo, every caption. Is this how we look after our most loyal customer?"
           />
           <Faq
             headline="How much does Quiet Corner software cost me?"
-            paragraphOne="It's free."
+            paragraphOne="The basic version is free to use. Please contact us for plug-in details."
           />
           <Faq
             headline="What commission does Quiet Corner take?"
-            paragraphOne="Quiet Corner takes 10% of revenue. There are Stripe charges also associated."
+            paragraphOne="Quiet Corner takes 5%. The publisher takes 95% of revenue, less Stripe transaction fees. "
           />
           <Faq
             headline="How should I price my story?"
@@ -31,7 +36,7 @@ const SecondPage = () => (
           />
           <Faq
             headline="I want to include advertising on my story. How do I?"
-            paragraphOne="Sorry, Quiet Corner exists to preserve the art of storytelling. It’s about a culturally ravenous audience investing in quality. No fluff, no ads."
+            paragraphOne="Sorry, we’re about a streamlined and seamless viewing experience. Our software strips out all ads. "
           />
           <Faq
             headline="Should I disable the paywall after a number of days?"
@@ -43,11 +48,19 @@ const SecondPage = () => (
           />
           <Faq
             headline="Can you put a full-time subscription paywall on my site?"
-            paragraphOne="No, we want our audiences to feel guilty in the same way you do when you have a gym membership when you’re not using it enough. No one likes direct debits. We don’t do that here. We’re pay-by-play. Quiet Corner is a micro-paywall that allows you to buy the best media on your phone or desktop – without ads."
+            paragraphOne="No, we don't want our audiences to feel guilty in the same way you do when you have a gym membership when you’re not using it enough. No one likes direct debits. We don’t do that here. We’re pay-by-play. Quiet Corner is a micro-paywall that allows you to buy the best media on your phone or desktop – without ads."
           />
           <Faq
-            headline="Will Quiet Corner piss my reader’s off?"
-            paragraphOne="Your work has likely always been free. Now it’s not. This will almost certainly create concern with your readers. However, we’re challenging the status quo that all media is free. Which is a desperately unsustainable model. We’re about providing a platform to create the work we’re proud of. It’s not clickbait, it’s not a race to the bottom, it’s the work that represents us at our best. It’s for our loyalists."
+            headline="Will Quiet Corner turn my reader’s off?"
+            paragraphOne="Your work has likely always been free. Now it’s not. This will almost certainly create concern with your readers. However, we’re challenging the status quo that all media is free. Which is a desperately unsustainable model. We’re trying to provide a platform to create the work we’re proud of. It’s not the number of page views, it’s not a race to the bottom, it’s the work that represents us at our best. It’s for our brand’s loyalists."
+           />
+           <Faq
+           headline="How do I see how income from my stories?"
+           paragraphOne="Login to your dashboard and it will deliver you live updates. "
+           />
+           <Faq
+           headline="How do I access stories I’ve bought?"
+           paragraphOne="They’re live on the site where you purchased them. Or, you can login to your dashboard to see the stories you’ve bought."
            />
         </div>
       </div>
